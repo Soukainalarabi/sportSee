ac51514fc3d701515b53682231a9ca5acd08ec0e
Soukaina Larabi <Soukaina.larabi44@gmail.com>
2023-09-06 15:21:01 +0200

    done

--- src/components/LineChart/index.jsx
+++ src/components/LineChart/index.jsx
@@ -5,15 +5,24 @@ import {
 import PropTypes from 'prop-types';
 
 function formatXAxis(value) {
-  if (value === 1) return 'L';
-  if (value === 2) return 'M';
-  if (value === 3) return 'M';
-  if (value === 4) return 'J';
-  if (value === 5) return 'V';
-  if (value === 6) return 'S';
-  if (value === 7) return 'D';
-
-  return value;
+  switch (value) {
+    case 1:
+      return 'L';
+    case 2:
+      return 'M';
+    case 3:
+      return 'M';
+    case 4:
+      return 'J';
+    case 5:
+      return 'V';
+    case 6:
+      return 'S';
+    case 7:
+      return 'D';
+    default:
+      return value;
+  }
 }
 function CustomTooltipLine({ active, payload }) {
   if (active && payload && payload.length) {
